33abfc236dec2901cae3a7d3221e4f16b8190901
Dan Green <danngreen1@gmail.com>
2026-02-19 10:08:45 -0800

    flashsd script is safer

--- flashsd.sh
+++ flashsd.sh
@@ -1,1 +1,1 @@
-sudo dd if=build/stm32mp2/release/tf-a-stm32mp257f-ev1.stm32 of=/dev/disk4s1 && sudo dd if=build/stm32mp2/release/fip.bin of=/dev/disk4s5
+[[ -n "$SDCARDDEV" ]] && sudo dd if=build/stm32mp2/release/tf-a-stm32mp257f-ev1.stm32 of=${SDCARDDEV}1 && sudo dd if=build/stm32mp2/release/fip.bin of=${SDCARDDEV}5 || echo "SDCARDDEV is not set"
