59fb21857784573ed0b7b20d0bf6ad894dd2fbff
Dan Green <danngreen1@gmail.com>
2026-07-09 16:30:53 -0700

    buidlfiptool script is OS-aware

--- buildfiptool.sh
+++ buildfiptool.sh
@@ -1,2 +1,10 @@
-make fiptool PLAT=stm32mp2 BAREMETAL_IMAGE_LOADER=1 OPENSSL_DIR=/opt/homebrew/opt/openssl@1.1 HOSTCCFLAGS="-I/opt/homebrew/opt/openssl@1.1/include"
+#!/usr/bin/env bash
+# Build the fiptool host tool.
+# On macOS, point the build at Homebrew's OpenSSL; on Linux the system
+# OpenSSL headers (package libssl-dev) are found automatically.
 
+if [ "$(uname -s)" = "Darwin" ]; then
+	make fiptool PLAT=stm32mp2 BAREMETAL_IMAGE_LOADER=1 OPENSSL_DIR=/opt/homebrew/opt/openssl@1.1 HOSTCCFLAGS="-I/opt/homebrew/opt/openssl@1.1/include"
+else
+	make fiptool PLAT=stm32mp2 BAREMETAL_IMAGE_LOADER=1
+fi
